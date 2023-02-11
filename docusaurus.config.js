f580aa9785f5f41c2286338ee72fa75532dfb041
Faiz Adi Wicaksono <faizadiwicaksono@hotmail.com>
2023-02-11 15:55:40 +0700

    feat: penambahan fitur pencarian
    Menambah themes easyops-cn/docusaurus-search-local di konfigurasi.
    
    Setting index untuk skip Blog.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,6 +66,21 @@ const config = {
     ],
   ],
 
+  themes: [
+    // ... Your other themes.
+    [
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        indexBlog: false,
+        docsRouteBasePath: "/",
+      }),
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
